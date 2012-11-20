5b3ebd9a25aa78410843bfda9bfd3528b07d094e
Melissa Gonzalez <meliand16@gmail.com>
2012-11-20 03:35:24 -0600

    casi lista maquina virtual

--- fun.py
+++ fun.py
@@ -1,25 +1,16 @@
 from mem import *
 
-"""Class that represents an Activation Record"""
 class Funciones:
     def __init__(self):
         self.ieje = 0
-        self.memlocal = Memory(40000)
-        self.memtemp = Memory(120000)
+        self.memlocal = Memoria(40000)
+        self.memtemp = Memoria(120000)
         self.param = []
-        self.return_value = 0
+        self.valor = 0    
     
-    
-    
-    def setParam(self, address, value, past_ar, mwild):
-        if type(value) == type([]):
-            for i in range(len(value)):
-                elem = value[i]
-                if mem_type(elem) == type([]):                  # If 150001 or 203001
-                    value[i] = self.swap_memories(elem, past_ar, mwild)
-        
-        self.param.append(address)
-        self.memlocal.set(address, value)   
+    def setParam(self, direccion, valor, arrtemp, memresto):        
+        self.param.append(direccion)
+        self.memlocal.set(direccion, valor)
     
     def getParam(self, k):
         return self.param[k]
